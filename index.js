d66051c9080f797c8a18c306f4bd54c4de6324fd
sean lee <freshdried@gmail.com>
2017-12-14 00:06:13 -0600

    Indentation lists (#5)
    * IList wip
    
    * round trip init
    
    * extra spaces
    
    * pretty printing
    
    * pretty rename

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ interface AST {
   children?: Array<AST>;
   print(): string;
   pretty(): AST;
+  _pretty(indent: number): AST;
 }
 
 class Name implements AST {
@@ -17,6 +18,9 @@ class Name implements AST {
     return this.data;
   }
   pretty() {
+    return this._pretty(0);
+  }
+  _pretty(indent: number) {
     return new Name(this.data);
   }
 }
@@ -40,10 +44,48 @@ class List implements AST {
     return str;
   }
   pretty() {
+    return this._pretty(0);
+  }
+  _pretty(indent: number) {
     const spaces = Array(this.children.length + 1).fill(" ");
     spaces[0] = "";
     spaces[this.children.length] = "";
-    return new List(this.children.map(x => x.pretty()), spaces);
+    return new List(this.children.map(x => x._pretty(indent)), spaces);
+  }
+}
+
+class IList implements AST {
+  type = "IList";
+  children: Array<AST>;
+  indent: number;
+  extraSpaces: Array<string>;
+  constructor(
+    children: Array<AST>,
+    indent: number,
+    extraSpaces: Array<string>
+  ) {
+    this.children = [...children];
+    this.indent = indent;
+    this.extraSpaces = extraSpaces;
+  }
+  print() {
+    let str = this.children[0].print();
+    for (let i = 1; i < this.children.length; i += 1) {
+      str += "\n";
+      str += this.extraSpaces[i - 1];
+      str += " ".repeat(this.indent);
+      str += this.children[i].print();
+    }
+    return str;
+  }
+  pretty() {
+    return this._pretty(0);
+  }
+  _pretty(indent: number) {
+    const children = this.children.map(x => x._pretty(indent + 2));
+    const newIndent = indent + 2;
+    const extraSpaces = Array(this.extraSpaces.length).fill("");
+    return new IList(children, newIndent, extraSpaces);
   }
 }
 
@@ -57,13 +99,16 @@ class NumLit implements AST {
     return `${this.data}`;
   }
   pretty() {
+    return this._pretty(0);
+  }
+  _pretty(indent: number) {
     return new NumLit(this.data);
   }
 }
 
 type State = {
   text: string,
-  indent: number
+  indents: Array<number>
 };
 
 export class Ctx {
@@ -71,19 +116,19 @@ export class Ctx {
   constructor(state: State) {
     this.state = state;
   }
-  getText(): string{
+  getText(): string {
     return this.state.text;
   }
-  setText(next: string): void{
+  setText(next: string): void {
     this.state.text = next;
   }
-  getIndent(): number{
-    return this.state.indent;
+  getIndents(): Array<number> {
+    return this.state.indents;
   }
-  setIndent(next: number): void{
-    this.state.indent = next;
+  setIndents(next: Array<number>): void {
+    this.state.indents = next;
   }
-  run<B>(parser: Parser<B>): B {
+  run<A>(parser: Parser<A>): A {
     return parser(this);
   }
 }
@@ -114,52 +159,150 @@ export const parseNumLit: Parser<AST> = ctx => {
   return new NumLit(parseInt(numString));
 };
 
-export const constant = (substring: string): Parser<void> => ctx => {
+export const constant = (substring: string): Parser<string> => ctx => {
   const text = ctx.getText();
   const i = text.indexOf(substring);
   if (i != 0) {
-    throw new ParseError("could not find constant");
+    throw new ParseError(
+      `could not find constant ${JSON.stringify(substring)}`
+    );
   }
   ctx.setText(text.substring(i + substring.length));
-  return;
+  return substring;
 };
 
 export function alternate<O>(options: Array<Parser<O>>): Parser<O> {
   return ctx => {
     for (const f of options) {
       const text = ctx.getText();
-      const indent = ctx.getIndent();
+      const indents = ctx.getIndents();
       try {
         return f(ctx);
       } catch (e) {
         ctx.setText(text);
-        ctx.setIndent(indent);
+        ctx.setIndents(indents);
       }
     }
     throw new ParseError("alternation failed");
   };
 }
 
-
-
 export function parse(input: string): AST {
-  const ctx = new Ctx({ text: input, indent: 0 });
+  const ctx = new Ctx({ text: input, indents: [0] });
   return parseTerm0(ctx);
 }
 
-const optionalWhitespace: Parser<string> = ctx => {
+const regExp = (r: RegExp): Parser<string> => ctx => {
   const text = ctx.getText();
-  const match = text.match(/^\s*/);
+  const match = text.match(r);
   if (!match) {
-    return "";
+    throw new Error("no match");
   }
   ctx.setText(text.substring(match[0].length));
   return match[0];
 };
 
+const withDefault = function<A>(parser: Parser<A>, def: A): Parser<A> {
+  return alternate([parser, () => def]);
+};
+
+const getSpaces: Parser<string> = regExp(/^\ +/);
+const getNewline: Parser<string> = constant("\n");
+
+const maybe = function<A>(parser: Parser<A>): Parser<?A> {
+  return alternate([ctx => parser(ctx), () => undefined]);
+};
+const whitespace: Parser<string> = regExp(/^\s+/);
+const optionalWhitespace: Parser<string> = withDefault(whitespace, "");
+
+const headIndent: Parser<number> = ctx => {
+  const indents = ctx.getIndents();
+  return indents[indents.length - 1];
+};
+
+const pushIndent = (numSpaces: number): Parser<void> => ctx => {
+  const indents = ctx.getIndents();
+  ctx.setIndents([...indents, numSpaces]);
+};
+
+const popIndent: Parser<void> = ctx => {
+  const indents = ctx.getIndents();
+  const newIndents = [...indents];
+  newIndents.pop();
+  ctx.setIndents(newIndents);
+};
+
+const increasedIndentationNewline: Parser<{
+  extraSpace: string,
+  indent: number
+}> = ctx => {
+  const currentIndent = ctx.run(headIndent);
+  ctx.run(maybe(getSpaces));
+  ctx.run(getNewline);
+  const extraSpace = ctx.run(withDefault(regExp(/^\s*\n+/), ""));
+  const numSpaces = ctx.run(getSpaces).length;
+  if (numSpaces > currentIndent) {
+    ctx.run(pushIndent(numSpaces));
+    return { extraSpace, indent: numSpaces };
+  }
+  throw new Error("no increased indentation");
+};
+
+const sameIndentationNewline: Parser<{
+  extraSpace: string,
+  indent: number
+}> = ctx => {
+  const currentIndent = ctx.run(headIndent);
+  ctx.run(maybe(getSpaces));
+  ctx.run(getNewline);
+  const extraSpace = ctx.run(withDefault(regExp(/^\s*\n+/), ""));
+  const numSpaces = ctx.run(getSpaces).length;
+  if (numSpaces === currentIndent) {
+    return { extraSpace, indent: numSpaces };
+  }
+  throw new Error("not same indentation");
+};
+
+export const parseIList: Parser<AST> = ctx => {
+  const term = ctx.run(parseTerm1);
+  const { extraSpace, indent } = ctx.run(increasedIndentationNewline);
+  const child = ctx.run(parseTerm0);
+  const { children, extraSpaces } = ctx.run(
+    parseIListAux({ children: [child], extraSpaces: [extraSpace] })
+  );
+  return new IList([term, ...children], indent, extraSpaces);
+};
+
+function parseIListAux({
+  children,
+  extraSpaces
+}: {
+  children: Array<AST>,
+  extraSpaces: Array<string>
+}): Parser<{ children: Array<AST>, extraSpaces: Array<string> }> {
+  return alternate([
+    ctx => {
+      const { extraSpace } = ctx.run(sameIndentationNewline);
+      const child = ctx.run(parseTerm0);
+      return ctx.run(
+        parseIListAux({
+          children: [...children, child],
+          extraSpaces: [...extraSpaces, extraSpace]
+        })
+      );
+    },
+    ctx => {
+      ctx.run(popIndent);
+      return { children, extraSpaces };
+    }
+  ]);
+}
+
 export const parseList: Parser<AST> = ctx => {
   ctx.run(constant("("));
-  const { children, spaces } = ctx.run(parseListAux({ children: [], spaces: [] }));
+  const { children, spaces } = ctx.run(
+    parseListAux({ children: [], spaces: [] })
+  );
   return new List(children, spaces);
 };
 
@@ -172,21 +315,25 @@ function parseListAux({
 }): Parser<{ children: Array<AST>, spaces: Array<string> }> {
   return ctx => {
     const space = ctx.run(optionalWhitespace);
-    return ctx.run(alternate([
-      ctx => {
-        ctx.run(constant(")"));
-        return { children, spaces: [...spaces, space] };
-      },
-      ctx => {
-        const child = ctx.run(parseTerm0);
-        return ctx.run(parseListAux({
-          children: [...children, child],
-          spaces: [...spaces, space]
-        }));
-      }
-    ]))
-  }
+    return ctx.run(
+      alternate([
+        ctx => {
+          ctx.run(constant(")"));
+          return { children, spaces: [...spaces, space] };
+        },
+        ctx => {
+          const child = ctx.run(parseTerm0);
+          return ctx.run(
+            parseListAux({
+              children: [...children, child],
+              spaces: [...spaces, space]
+            })
+          );
+        }
+      ])
+    );
+  };
 }
 
 const parseTerm1: Parser<AST> = alternate([parseList, parseName, parseNumLit]);
-const parseTerm0: Parser<AST> = alternate([parseTerm1]);
+const parseTerm0: Parser<AST> = alternate([parseIList, parseTerm1]);
--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
 // @flow
-import { Ctx, parse, parseName, parseNumLit, parseList } from "./";
+import { Ctx, parse, parseName, parseNumLit, parseList, parseIList } from "./";
 
 describe("parseName", () => {
   it("parses names", () => {
-    const ctx = new Ctx({ text: "foo 1", indent: 0 });
+    const ctx = new Ctx({ text: "foo 1", indents: [0] });
     const result = parseName(ctx);
     expect(result).toEqual({
       type: "Name",
@@ -12,42 +12,205 @@ describe("parseName", () => {
   });
 
   it("fails on whitespace", () => {
-    const ctx = new Ctx({ text: "   foo 1", indent: 0 });
+    const ctx = new Ctx({ text: "   foo 1", indents: [0] });
     expect(() => parseName(ctx)).toThrow("not a name");
   });
 });
 
 describe("parseNumLit", () => {
   it("parses numeric literals", () => {
-    const ctx = new Ctx({ text: "100 asdf", indent: 0 });
+    const ctx = new Ctx({ text: "100 asdf", indents: [0] });
     const result = parseNumLit(ctx);
     expect(result).toEqual({ type: "NumLit", data: 100 });
   });
 
   it("fails on whitespace", () => {
-    const input = new Ctx({ text: "  100 asdf", indent: 0 });
+    const input = new Ctx({ text: "  100 asdf", indents: [0] });
     expect(() => parseNumLit(input)).toThrow("not a number");
   });
 });
 
 describe("parseList", () => {
   it("parses lists", () => {
-    const ctx = new Ctx({ text: `(foo 1 2 1)`, indent: 0 });
+    const ctx = new Ctx({ text: `(foo 1 2 1)`, indents: [0] });
     const result = parseList(ctx);
     expect(result).toEqual({
-        type: "List",
-        spaces: ["", " ", " ", " ", ""],
-        children: [
-          { type: "Name", data: "foo" },
-          { type: "NumLit", data: 1 },
-          { type: "NumLit", data: 2 },
-          { type: "NumLit", data: 1 }
-        ]
+      type: "List",
+      spaces: ["", " ", " ", " ", ""],
+      children: [
+        { type: "Name", data: "foo" },
+        { type: "NumLit", data: 1 },
+        { type: "NumLit", data: 2 },
+        { type: "NumLit", data: 1 }
+      ]
     });
   });
 });
 
-describe("round trip identity laws", () => {
+describe("parseIList", () => {
+  it("parses indentation-based lists", () => {
+    const text = `foo
+  1
+  2`;
+    const ctx = new Ctx({ text, indents: [0] });
+    const result = parseIList(ctx);
+    expect(result).toEqual({
+      type: "IList",
+      indent: 2,
+      extraSpaces: ["", ""],
+      children: [
+        { type: "Name", data: "foo" },
+        { type: "NumLit", data: 1 },
+        { type: "NumLit", data: 2 }
+      ]
+    });
+  });
+  it("parses indentation-based lists with one child", () => {
+    const text = `foo
+  bar`;
+    const ctx = new Ctx({ text, indents: [0] });
+    const result = parseIList(ctx);
+    expect(result).toEqual({
+      type: "IList",
+      indent: 2,
+      extraSpaces: [""],
+      children: [{ type: "Name", data: "foo" }, { type: "Name", data: "bar" }]
+    });
+  });
+
+  it("parses indentation-based lists with one child, with an extra new line", () => {
+    const text = `foo
+
+  bar`;
+    const ctx = new Ctx({ text, indents: [0] });
+    const result = parseIList(ctx);
+    expect(result).toEqual({
+      type: "IList",
+      indent: 2,
+      extraSpaces: ["\n"],
+      children: [{ type: "Name", data: "foo" }, { type: "Name", data: "bar" }]
+    });
+  });
+
+  it("parses indentation-based lists with one child, with two extra new lines", () => {
+    const text = `foo
+
+
+  bar`;
+    const ctx = new Ctx({ text, indents: [0] });
+    const result = parseIList(ctx);
+    expect(result).toEqual({
+      type: "IList",
+      indent: 2,
+      extraSpaces: ["\n\n"],
+      children: [{ type: "Name", data: "foo" }, { type: "Name", data: "bar" }]
+    });
+  });
+
+  it("parses nested indentation-based lists", () => {
+    const text = `foo
+  bar
+    baz
+    1
+  2`;
+    const ctx = new Ctx({ text, indents: [0] });
+    const result = parseIList(ctx);
+    expect(result).toEqual({
+      type: "IList",
+      indent: 2,
+      extraSpaces: ["", ""],
+      children: [
+        { type: "Name", data: "foo" },
+        {
+          type: "IList",
+          indent: 4,
+          extraSpaces: ["", ""],
+          children: [
+            { type: "Name", data: "bar" },
+            { type: "Name", data: "baz" },
+            { type: "NumLit", data: 1 }
+          ]
+        },
+        { type: "NumLit", data: 2 }
+      ]
+    });
+  });
+
+  it("parses super nested indentation-based lists", () => {
+    const text = `foo
+  bar
+    baz
+      1
+  2`;
+    const ctx = new Ctx({ text, indents: [0] });
+    const result = parseIList(ctx);
+    expect(result).toEqual({
+      type: "IList",
+      indent: 2,
+      extraSpaces: ["", ""],
+      children: [
+        { type: "Name", data: "foo" },
+        {
+          type: "IList",
+          indent: 4,
+          extraSpaces: [""],
+          children: [
+            { type: "Name", data: "bar" },
+            {
+              type: "IList",
+              indent: 6,
+              extraSpaces: [""],
+              children: [
+                { type: "Name", data: "baz" },
+                { type: "NumLit", data: 1 }
+              ]
+            }
+          ]
+        },
+        { type: "NumLit", data: 2 }
+      ]
+    });
+  });
+  it("parses super nested indentation-based lists, with weird indentation", () => {
+    const text = `foo
+    bar
+     baz
+          1
+          2
+    2`;
+    const ctx = new Ctx({ text, indents: [0] });
+    const result = parseIList(ctx);
+    expect(result).toEqual({
+      type: "IList",
+      indent: 4,
+      extraSpaces: ["", ""],
+      children: [
+        { type: "Name", data: "foo" },
+        {
+          type: "IList",
+          indent: 5,
+          extraSpaces: [""],
+          children: [
+            { type: "Name", data: "bar" },
+            {
+              type: "IList",
+              indent: 10,
+              extraSpaces: ["", ""],
+              children: [
+                { type: "Name", data: "baz" },
+                { type: "NumLit", data: 1 },
+                { type: "NumLit", data: 2 }
+              ]
+            }
+          ]
+        },
+        { type: "NumLit", data: 2 }
+      ]
+    });
+  });
+});
+
+describe("parse/print round trip identity laws", () => {
   function testRoundTrip(input: string) {
     test(JSON.stringify(input), () => {
       const output = parse(input);
@@ -59,9 +222,22 @@ describe("round trip identity laws", () => {
   testRoundTrip("( foo bar )");
   testRoundTrip("( foo bar  baz )");
   testRoundTrip("( foo (foo 1 2 3 ) baz )");
+
+  testRoundTrip(`foo
+  1`);
+  testRoundTrip(`foo
+  1
+
+  foo
+
+
+    bar
+      sdf
+
+  baz`);
 });
 
-describe("pretty printing", () => {
+describe("pretty printing properties", () => {
   function testPretty(input: string, expected: string) {
     test(JSON.stringify(input), () => {
       const output = parse(input);
@@ -74,4 +250,24 @@ describe("pretty printing", () => {
   testPretty("(foo bar)", "(foo bar)");
   testPretty("( foo  bar )", "(foo bar)");
   testPretty("( foo (foo 1 2 3 ) baz )", "(foo (foo 1 2 3) baz)");
+
+  testPretty(
+    `foo
+  1
+
+  foo
+
+
+    bar
+      sdf
+
+
+  baz`,
+    `foo
+  1
+  foo
+    bar
+      sdf
+  baz`
+  );
 });
