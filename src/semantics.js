1b099ba4a85c699014e3f5e8ab9fc516fbbd8b19
sean lee <freshdried@gmail.com>
2017-12-16 20:21:38 -0600

    Pattern semantics (#10)
    * shift tests
    
    * scale
    
    * slow fast
    
    * branch
    
    * wip
    
    * ooo
    
    * :( functional representation is much easier to grapple...
    
    * switch to functional, gateing
    
    * semantics

---
+++ src/semantics.js
@@ -0,0 +1,119 @@
+// @flow
+
+type Event<A> = {
+  start: number,
+  end: number,
+  value: A
+};
+
+function mapTime<A>(f: number => number): (Event<A>) => Event<A> {
+  return event => ({
+    ...event,
+    start: f(event.start),
+    end: f(event.end)
+  });
+}
+
+export type Pattern<A> = (start: number, end: number) => Array<Event<A>>;
+
+export function periodic<A>({
+  period,
+  duration,
+  phase,
+  value
+}: {
+  period: number,
+  duration: number,
+  phase: number,
+  value: A
+}): Pattern<A> {
+  return (start, end) => {
+    let events = [];
+    for (let i = Math.floor(start / period) * period; i < end; i += period) {
+      const time = i + phase;
+      if (!(time >= start && time < end)) {
+        continue;
+      }
+      events.push({ start: time, end: time + duration, value });
+    }
+    return events;
+  };
+}
+
+export const beat: Pattern<void> = periodic({
+  period: 1,
+  duration: 1,
+  phase: 0,
+  value: undefined
+});
+
+export function query<A>(
+  start: number,
+  end: number,
+  pattern: Pattern<A>
+): Array<Event<A>> {
+  return pattern(start, end);
+}
+
+export function stack<A>(children: Array<Pattern<A>>): Pattern<A> {
+  return (start, end) => {
+    let events = [];
+    for (let i = 0; i < children.length; i += 1) {
+      events = events.concat(query(start, end, children[i]));
+    }
+    return events.sort((a, b) => a.start - b.start);
+  };
+}
+
+type Transformer<A> = (Pattern<A>) => Pattern<A>;
+
+export function shift<A>(n: number): Transformer<A> {
+  return pattern => (start, end) => {
+    return pattern(start - n, end - n).map(mapTime(x => x + n));
+  };
+}
+
+export function slow<A>(k: number): Transformer<A> {
+  return pattern => (start, end) => {
+    return pattern(start / k, end / k).map(mapTime(x => x * k));
+  };
+}
+
+export function fast<A>(k: number): Transformer<A> {
+  return slow(1 / k);
+}
+
+function gateWith<A>(gatePat: Pattern<any>): Transformer<A> {
+  return pattern => (start, end) => {
+    const gates = query(start, end, gatePat);
+    let events = [];
+    for (const { start, end } of gates) {
+      events = events.concat(query(start, end, pattern));
+    }
+    return events;
+  };
+}
+
+export function gate<A>(phase: number, period: number): Transformer<A> {
+  return gateWith(
+    periodic({ period: period, duration: 1, phase, value: undefined })
+  );
+}
+
+export const silence: Pattern<any> = () => [];
+
+export function alt<A>(children: Array<Transformer<A>>): Transformer<A> {
+  return pattern => {
+    return stack(
+      children.map((transform, i) => {
+        return gate(i, children.length)(transform(pattern));
+      })
+    );
+  };
+}
+
+export function and<A>(transform: Transformer<A>): Transformer<A> {
+  return pattern => {
+    return stack([pattern, transform(pattern)]);
+  };
+}
---
+++ src/semantics.test.js
@@ -0,0 +1,189 @@
+// @flow
+import {
+  type Pattern,
+  query,
+  shift,
+  fast,
+  slow,
+  beat,
+  stack,
+  periodic,
+  gate,
+  alt,
+  silence,
+  and
+} from "./semantics";
+
+describe("beat", () => {
+  const pattern = beat;
+
+  testQuery(pattern, 0, 1, [0]);
+  testQuery(pattern, 0, 2, [0, 1]);
+  testQuery(pattern, 0, 0.5, [0]);
+  testQuery(pattern, 0.5, 1, []);
+  testQuery(pattern, 1, 2, [1]);
+  testQuery(pattern, 0.5, 1.5, [1]);
+  testQuery(pattern, 0.5, 2.5, [1, 2]);
+});
+
+describe("shift(0.5)(beat)", () => {
+  const pattern = shift(0.5)(beat);
+
+  testQuery(pattern, 0, 1, [0.5]);
+  testQuery(pattern, 0, 2, [0.5, 1.5]);
+  testQuery(pattern, 0, 0.5, []);
+  testQuery(pattern, 0.5, 1, [0.5]);
+  testQuery(pattern, 1, 2, [1.5]);
+  testQuery(pattern, 0.5, 1.5, [0.5]);
+  testQuery(pattern, 0.5, 2.5, [0.5, 1.5]);
+});
+
+describe("slow(2)(beat)", () => {
+  const pattern = slow(2)(beat);
+
+  testQuery(pattern, 0, 1, [0]);
+  testQuery(pattern, 0, 2, [0]);
+  testQuery(pattern, 0, 4, [0, 2]);
+  testQuery(pattern, 0, 0.5, [0]);
+  testQuery(pattern, 0.5, 1, []);
+  testQuery(pattern, 1, 2, []);
+  testQuery(pattern, 0.5, 1.5, []);
+  testQuery(pattern, 0.5, 2.5, [2]);
+});
+
+describe("fast(2)(beat)", () => {
+  const pattern = fast(2)(beat);
+
+  testQuery(pattern, 0, 1, [0, 0.5]);
+  testQuery(pattern, 0, 2, [0, 0.5, 1, 1.5]);
+  testQuery(pattern, 0, 0.5, [0]);
+  testQuery(pattern, 0.5, 1, [0.5]);
+  testQuery(pattern, 1, 2, [1, 1.5]);
+  testQuery(pattern, 0.5, 1.5, [0.5, 1]);
+  testQuery(pattern, 0.5, 2.5, [0.5, 1, 1.5, 2]);
+});
+
+const bd = periodic({ period: 1, duration: 1, phase: 0, value: "bd" });
+
+const sn = periodic({ period: 1, duration: 1, phase: 0, value: "sn" });
+
+describe("stack", () => {
+  describe("stack", () => {
+    const pattern = stack([bd, sn]);
+    testQuery(pattern, 0, 1, [0, 0], ["bd", "sn"]);
+    testQuery(pattern, 0, 2, [0, 0, 1, 1], ["bd", "sn", "bd", "sn"]);
+  });
+
+  describe("slow stack", () => {
+    const pattern = slow(2)(stack([bd, sn]));
+    testQuery(pattern, 0, 1, [0, 0], ["bd", "sn"]);
+    testQuery(pattern, 0, 2, [0, 0], ["bd", "sn"]);
+  });
+
+  describe("shift stack", () => {
+    const pattern = shift(0.5)(stack([bd, sn]));
+    testQuery(pattern, 0, 1, [0.5, 0.5], ["bd", "sn"]);
+    testQuery(pattern, 0, 2, [0.5, 0.5, 1.5, 1.5], ["bd", "sn", "bd", "sn"]);
+  });
+});
+
+describe("gate", () => {
+  describe("beat; gate(0, 4)", () => {
+    const pattern = gate(0, 4)(beat);
+    testQuery(pattern, 0, 4, [0]);
+    testQuery(pattern, 0, 8, [0, 4]);
+    testQuery(pattern, 1, 4, []);
+    testQuery(pattern, 1, 8, [4]);
+  });
+
+  describe("beat; gate(1, 4)", () => {
+    const pattern = gate(1, 4)(beat);
+    testQuery(pattern, 0, 4, [1]);
+    testQuery(pattern, 0, 8, [1, 5]);
+    testQuery(pattern, 1, 4, [1]);
+    testQuery(pattern, 1, 8, [1, 5]);
+  });
+
+  describe("beat; gate(1, 4) ; shift 1", () => {
+    const pattern = shift(1)(gate(1, 4)(beat));
+    testQuery(pattern, 0, 4, [2]);
+    testQuery(pattern, 0, 8, [2, 6]);
+    testQuery(pattern, 1, 4, [2]);
+    testQuery(pattern, 1, 8, [2, 6]);
+  });
+
+  describe("beat; gate(1, 4) ; shift 1", () => {
+    const pattern = shift(2)(gate(1, 4)(beat));
+    testQuery(pattern, 0, 4, [3]);
+    testQuery(pattern, 0, 8, [3, 7]);
+    testQuery(pattern, 1, 4, [3]);
+    testQuery(pattern, 1, 8, [3, 7]);
+  });
+
+  describe("beat; gate(1, 4) ; fast 2", () => {
+    const pattern = fast(2)(gate(1, 4)(beat));
+    testQuery(pattern, 0, 2, [0.5]);
+    testQuery(pattern, 0, 4, [0.5, 2.5]);
+    testQuery(pattern, 0, 8, [0.5, 2.5, 4.5, 6.5]);
+    testQuery(pattern, 1, 4, [2.5]);
+    testQuery(pattern, 1, 8, [2.5, 4.5, 6.5]);
+  });
+});
+
+describe("alt", () => {
+  describe("alt bd sn", () => {
+    const pattern = alt([() => bd, () => sn])(silence);
+    testQuery(pattern, 0, 1, [0], ["bd"]);
+    testQuery(pattern, 0, 2, [0, 1], ["bd", "sn"]);
+    testQuery(pattern, 0, 4, [0, 1, 2, 3], ["bd", "sn", "bd", "sn"]);
+    testQuery(pattern, 1, 3, [1, 2], ["sn", "bd"]);
+  });
+
+  describe("alt (alt bd sn) sn", () => {
+    const pattern = alt([alt([() => bd, () => sn]), () => sn])(silence);
+    testQuery(pattern, 0, 1, [0], ["bd"]);
+    testQuery(pattern, 0, 2, [0, 1], ["bd", "sn"]);
+    testQuery(pattern, 0, 4, [0, 1, 2, 3], ["bd", "sn", "bd", "sn"]);
+    testQuery(pattern, 1, 3, [1, 2], ["sn", "bd"]);
+  });
+
+  describe("alt (alt bd sn; fast 2) sn", () => {
+    const pattern = alt([x => fast(2)(alt([() => bd, () => sn])(x)), () => sn])(
+      silence
+    );
+    testQuery(pattern, 0, 1, [0, 0.5], ["bd", "sn"]);
+    testQuery(pattern, 0, 2, [0, 0.5, 1], ["bd", "sn", "sn"]);
+    testQuery(
+      pattern,
+      0,
+      4,
+      [0, 0.5, 1, 2, 2.5, 3],
+      ["bd", "sn", "sn", "bd", "sn", "sn"]
+    );
+    testQuery(pattern, 1, 3, [1, 2, 2.5], ["sn", "bd", "sn"]);
+  });
+});
+
+describe("and", () => {
+  describe("bd ; and sn", () => {
+    const pattern = and(() => sn)(bd);
+    testQuery(pattern, 0, 1, [0, 0], ["bd", "sn"]);
+    testQuery(pattern, 0, 2, [0, 0, 1, 1], ["bd", "sn", "bd", "sn"]);
+  });
+});
+
+function testQuery<A>(
+  pattern: Pattern<A>,
+  start: number,
+  end: number,
+  expectedTimes: Array<number>,
+  expectedValues?: Array<A>
+) {
+  test(`(${start}, ${end})`, () => {
+    const events = query(start, end, pattern);
+    expect(events.map(x => x.start)).toEqual(expectedTimes);
+    if (expectedValues) {
+      expect(events.map(x => x.value)).toEqual(expectedValues);
+    }
+  });
+}
